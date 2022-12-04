2c0238b207de8e54f88205e22f717799c9ac6960
Agustina saputra <agustina@znitech.dev>
2022-12-04 10:41:20 +0800

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
+
+export default function Home({ products }) {
+  return (
+    <div className="bg-gray-100">
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+      {/* header */}
+      <Header />
+      {/* main content */}
+      <main className="md:max-w-screen-2xl lg:max-w-full mx-auto">
+        <Banner />
+        <ProductFeed products={products} />
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      products,
+    },
+  };
+}
